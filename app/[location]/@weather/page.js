506afff2f616bd85d49a42c6912957b16caf9d0f
Mahmudul Islam Shuvo <mahmudulislammern@gmail.com>
2026-02-01 05:23:13 +0600

    Refactor WeatherPage to resolve location coordinates and handle errors with NoLocationFound component

--- app/[location]/@weather/page.js
+++ app/[location]/@weather/page.js
@@ -1,10 +1,18 @@
+import NoLocationFound from "@/components/NoLocationFound";
 import WeatherComponent from "@/components/WeatherComponent";
+import { getResolvedLatLong } from "@/lib/location-info";
 
-const WeatherPage = ({
+const WeatherPage = async ({
   params: { location },
   searchParams: { latitude, longitude },
 }) => {
-  return <WeatherComponent lat={latitude} lon={longitude} />;
+  const resolve = await getResolvedLatLong(location, latitude, longitude);
+
+  if (resolve?.lat && resolve?.lon) {
+    return <WeatherComponent lat={resolve.lat} lon={resolve.lon} />;
+  } else {
+    return <NoLocationFound location={location} />;
+  }
 };
 
 export default WeatherPage;
