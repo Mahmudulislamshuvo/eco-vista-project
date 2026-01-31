b5274320432abb4528a0bd176243418be51bbf5a
Mahmudul Islam Shuvo <mahmudulislammern@gmail.com>
2026-02-01 01:14:31 +0600

    Add AQIComonent and Card components for air quality data display

--- app/[location]/@aqi/page.js
+++ app/[location]/@aqi/page.js
@@ -1,5 +1,10 @@
-const AqiPage = () => {
-  return <div>AqiPage</div>;
+import AQIComonent from "@/components/AQIComonent";
+
+const AqiPage = ({
+  params: { location },
+  searchParams: { latitude, longitude },
+}) => {
+  return <AQIComonent lat={latitude} lon={longitude} />;
 };
 
 export default AqiPage;
